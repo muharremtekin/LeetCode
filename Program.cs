6ada26f84e4d1834db3cc48750e4c018e262a89d
Muharrem TEKİN <85758996+muharremtekin@users.noreply.github.com>
2023-12-07 16:22:05 +0300

    cevaba bakarak yaptım

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+// MergeStringsAlternately.MergeAlternately("qwe","asd");
+string word1 = "muharrem";
+string word2 = "ahmethan";
+
+string mergedRes = MergeStringsAlternately.MergeAlternately(word1,word2);
+
+Console.WriteLine(mergedRes);
