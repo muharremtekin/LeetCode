a8e98a48c5a4309b61682fd0cc5b9c056d69db7d
Muharrem TEKİN <85758996+muharremtekin@users.noreply.github.com>
2023-12-07 16:22:49 +0300

    mini refactor

--- solutions/MergeStringsAlternately.cs
+++ solutions/MergeStringsAlternately.cs
@@ -10,21 +10,14 @@ sealed class MergeStringsAlternately
         int l = Math.Min(w1, w2);
 
         for (int i = 0; i < l; i++)
-        {
             merged.Append(word1[i]).Append(word2[i]);
-        }
 
         if (w1 == w2)
-        {
             return merged.ToString();
-        }
+
         else if (w1 > w2)
-        {
             return merged.Append(word1.Substring(l)).ToString();
-        }
         else
-        {
             return merged.Append(word2.Substring(l)).ToString();
-        }
     }
 }
