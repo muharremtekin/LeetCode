7adf33a6cc7cbf08cdac88801e426f620cdc6e43
Muharrem TEKİN <85758996+muharremtekin@users.noreply.github.com>
2024-01-10 12:40:35 +0300

    sync2

--- Program.cs
+++ Program.cs
@@ -1,9 +1,1 @@
 ﻿
-
-// MergeStringsAlternately.MergeAlternately("qwe","asd");
-string word1 = "muharrem";
-string word2 = "ahmethan";
-
-string mergedRes = MergeStringsAlternately.MergeAlternately(word1,word2);
-
-Console.WriteLine(mergedRes);
