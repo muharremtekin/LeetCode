c82f3ca92be2cb4fc867263a3e5d26cdae1b4b0d
Muharrem TEKIN <muharrem.tekin@bilgeadam.com>
2024-10-03 13:42:06 +0300

    add MaxConsecutiveOnes3 not completed yet

---
+++ solutions/MaxConsecutiveOnes3.cs
@@ -0,0 +1,23 @@
+// Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+// Example 1:
+
+// Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+// Output: 6
+// Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+// Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+public class MaxConsecutiveOnes3
+{
+    public static int LongestOnes(int[] nums, int k)
+    {
+        int left = 0;
+        int right = nums.Length;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+
+        }
+        
+        return 0;
+    }
+}
