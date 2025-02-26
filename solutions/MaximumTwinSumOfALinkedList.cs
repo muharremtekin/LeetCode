d5c9bd5457f0ca09cc1f6faef2377aa96be5fc94
Muharrem TEKIN <muharrem.tekin@bilgeadam.com>
2025-02-26 15:04:24 +0300

    add solution for MaximumTwinSumOfALinkedList

--- solutions/MaximumTwinSumOfALinkedList.cs
+++ solutions/MaximumTwinSumOfALinkedList.cs
@@ -4,39 +4,55 @@ public class MaximumTwinSumOfALinkedList
     // For example, if n = 4, then node 0 is the twin of node 3, and node 1 is the twin of node 2. These are the only nodes with twins for n = 4.
     // The twin sum is defined as the sum of a node and its twin.
     // Given the head of a linked list with even length, return the maximum twin sum of the linked list.
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
-        }
-    }
 
-    /// <summary>
-    /// Calculates the sum of the two largest elements in a linked list.
-    /// </summary>
-    /// <param name="head">The head node of the linked list.</param>
-    /// <returns>The sum of the two largest elements in the linked list.</returns>
+    // public class ListNode
+    // {
+    //     public int val;
+    //     public ListNode next;
+    //     public ListNode(int val = 0, ListNode next = null)
+    //     {
+    //         this.val = val;
+    //         this.next = next;
+    //     }
+    // }
+
+
     public int PairSum(ListNode head)
     {
-        // en büyük iki elemanın toplamı
-        int first = int.MinValue;
-        int second = int.MinValue;
+        List<int> list = new();
+        DFS(head, list);
 
-        ListNode node = head;
+        // alttaki kod daha hızlı
+        
+        // var nextCell = head;
+        // var list = new List<int>();
+        // while(nextCell != null){
+        //     list.Add(nextCell.val);
+        //     nextCell= nextCell.next;
+        // }
 
-        while (node != null)
+        if (list.Count is 2)
+            return list[0] + list[1];
+
+        int maxSum = 0;
+        int right = list.Count - 1;
+        int n = list.Count / 2;
+
+        for (int i = 0; i < n; i++)
         {
-            if (node.val > first)
-                first = node.val;
-            else if (node.val >= second)
-                second = node.val;
+            if (list[i] + list[right] > maxSum)
+                maxSum = list[i] + list[right];
 
-            node = node.next;
+            right--;
         }
-        return first + second;
+
+        return maxSum;
+    }
+
+    void DFS(ListNode node, List<int> list)
+    {
+        if (node == null) return;
+        list.Add(node.val);
+        DFS(node.next, list);
     }
 }
