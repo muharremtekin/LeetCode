72f314b4c790f661899465d2a8095bbfcdc76bc7
Muharrem Tekin <muharremtekinn@outlook.com.tr>
2026-01-03 12:59:37 +0300

    Refactor LongestCommonPrefix implementation for clarity and efficiency

--- TopInterview150/Solutions/LongestCommonPrefix.cs
+++ TopInterview150/Solutions/LongestCommonPrefix.cs
@@ -4,16 +4,9 @@ public sealed class LongestCommonPrefixProblem
     // url: https://leetcode.com/problems/longest-common-prefix
     public static string LongestCommonPrefix(string[] strs)
     {
-        string commonPrefix = string.Empty;
-
-        for (int strIndex = 0; strIndex < strs.Length; strIndex++)
+        string commonPrefix = strs[0];
+        for (int strIndex = 1; strIndex < strs.Length; strIndex++)
         {
-            if (strIndex is 0)
-            {
-                commonPrefix = strs[0];
-                continue;
-            }
-
             string prefix = FindPrefix(commonPrefix, strs[strIndex]);
 
             if (string.IsNullOrWhiteSpace(prefix))
@@ -30,14 +23,13 @@ public sealed class LongestCommonPrefixProblem
 
     private static string FindPrefix(string commonPrefix, string str)
     {
-        int n = commonPrefix.Length >= str.Length ? str.Length : commonPrefix.Length;
-        int index = 0;
+        int n = Math.Min(commonPrefix.Length, str.Length); int index = 0;
         for (; index < n; index++)
         {
             if (commonPrefix[index] != str[index]) break;
         }
         char[] strArray = new char[index];
         Array.Copy(commonPrefix.ToCharArray(), strArray, index);
-        return new string(strArray);
+        return commonPrefix[..index];
     }
 }
