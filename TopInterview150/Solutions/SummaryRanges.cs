bb253c4dd4274ff5eec286754d92ce933a754a1c
Muharrem Tekin <muharremtekinn@outlook.com.tr>
2026-01-03 17:33:14 +0300

    Refactor SummaryRanges method for improved variable usage and clarity

--- TopInterview150/Solutions/SummaryRanges.cs
+++ TopInterview150/Solutions/SummaryRanges.cs
@@ -4,18 +4,18 @@ public sealed class SummaryRangesProblem
     public static IList<string> SummaryRanges(int[] nums)
     {
         List<string> result = new();
-        if (nums.Length == 0) return result;
+        int n = nums.Length;
+        if (n == 0) return result;
 
         int start = 0;
 
-        for (int i = 0; i < nums.Length; i++)
+        for (int i = 0; i < n; i++)
         {
-            if (i == nums.Length - 1 || nums[i] + 1 != nums[i + 1])
+            if (i == n - 1 || nums[i] + 1 != nums[i + 1])
             {
-                if (start == i)
-                    result.Add(nums[start].ToString());
-                else
-                    result.Add($"{nums[start]}->{nums[i]}");
+                result.Add(start == i
+                    ? nums[start].ToString()
+                    : string.Concat(nums[start], "->", nums[i]));
 
                 start = i + 1;
             }
