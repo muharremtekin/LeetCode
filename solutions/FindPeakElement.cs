3325ee17e462289b32f90b0f050dd6a229bac2f7
Muharrem Tekin <muharremtekinn@outlook.com.tr>
2026-02-07 13:58:18 +0300

    add FindPeakElement solution implementation

---
+++ solutions/FindPeakElement.cs
@@ -0,0 +1,17 @@
+public sealed class FindPeakElementSolution
+{
+    public int FindPeakElement(int[] nums)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left < right)
+        {
+            int mid = (left + right) / 2;
+            if (nums[mid] < nums[mid + 1]) left = mid + 1;
+            else right = mid;
+        }
+
+        return left;
+    }
+}
